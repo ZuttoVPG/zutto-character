70a9708279c413cec47e79814ca185ab8ca5321d
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2017-09-18 00:34:25 +0000

    Exception handler outputs text instead of HTML

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        return response((string)$e, 500);
     }
 }
