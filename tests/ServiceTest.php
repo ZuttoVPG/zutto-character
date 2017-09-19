dd7404151c582316b4c4fe1d2e7ce6d4ece8a6ab
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2017-09-19 03:32:13 +0000

    Tests now pass

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -30,25 +30,54 @@ class ServiceTest extends TestCase
         $resp->assertResponseOk();
 
         // Using hashes so it doesn't dump the whole blob on your screen when the test fails
-        $this->assertEquals(md5($duck), md5($this->response->getContent()));
+        $data = json_decode($this->response->getContent(), true);
+        $this->assertEquals(md5($duck), md5(base64_decode($data['image'])));
     } // end testPssthrough
 
     public function testPassthroughUrl()
     {
-        // @TODO - mock the HTTP client 
-        $this->assertEquals(1, 0);
+        $duck = file_get_contents('public/testImg/steve_the_awful_duck.png');
+
+        $resp = $this->json('POST', '/character', [
+            'client' => $this->makeScreenData(),
+            'baseImage' => [
+                'type' => 'url',
+                // @TODO - mock guzzle, this is awful
+                'image' => 'https://character-dev.zuttopets.com/testImg/steve_the_awful_duck.png', 
+            ],
+            'attachments' => [],
+        ]);
+
+        $resp->assertResponseOk();
+
+        // Using hashes so it doesn't dump the whole blob on your screen when the test fails
+        $data = json_decode($this->response->getContent(), true);
+        $this->assertEquals(md5($duck), md5(base64_decode($data['image'])));
     } // end testPassthroughUrl
 
     public function testAssetUrlWhitelist()
     {
-         // @TODO - mock the HTTP client & config 
-        $this->assertEquals(1, 0);
+        $resp = $this->json('POST', '/character', [
+            'client' => $this->makeScreenData(),
+            'baseImage' => [
+                'type' => 'url',
+                'image' => 'https://not-whitelisted.com'
+            ],
+            'attachments' => [],
+        ]);
+
+        $resp->assertResponseStatus(500);
+        $resp->seeJsonStructure(['errors' => ['baseImage.image']]);
     } // end testAssetUrlWhitelist
 
     public function testAttaching()
     {
         $duck_path = 'public/testImg/steve_the_awful_duck.png';
         $hat_path = 'public/testImg/sad_beanie.png';
+
+        $pre_merge = new Imagick($duck_path);
+        $pre_merge_pixel = $pre_merge->getImagePixelColor(41, 52)->getColor();
+        unset($pre_merge);
 
         $resp = $this->json('POST', '/character', [
             'client' => $this->makeScreenData(),
@@ -62,15 +91,20 @@ class ServiceTest extends TestCase
                         'type' => 'blob',
                         'image' => base64_encode(file_get_contents($hat_path)),
                     ],
-                    'x' => 0,
-                    'y' => 0,
-                    'z' => 0,
+                    'x' => 25,
+                    'y' => 44,
+                    'z' => 1,
                 ],
             ],
         ]);
 
         $resp->assertResponseOk();
-        // @TODO: do something clever to test the result, like look for pink on a spot that was black
-        $this->assertEquals(1, 0);
+        $data = json_decode($this->response->getContent(), true);
+
+        $merge = new Imagick();
+        $merge->readImageBlob(base64_decode($data['image']));
+        $merge_pixel = $merge->getImagePixelColor(41, 52)->getColor();
+       
+        $this->assertNotEquals($merge_pixel, $pre_merge_pixel); 
     } // end testAttaching
 } // end TestService
